0d4150f13ed0259aace72f71a5baf952099f9bce
wellingtondelimasilva1982 <wellingtondelimasilva1982@gmail.com>
2025-04-04 21:13:24 -0300

    Agenda 07

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +12,39 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = $"Latitude: {t.lat} \n" +
+                            $"Longitude: {t.lon} \n" +
+                            $"Nascer do Sol: {t.sunrise} \n" +
+                            $"Por do Sol: {t.sunset} \n" +
+                            $"Temperatura Máxima: {t.temp_max} \n" +
+                            $"Temperatura Mínima: {t.temp_min} \n";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        lbl_res.Text = dados_previsao;
+
+                    } else
+                    {
+                        lbl_res.Text = "Sem dados de previsão.";
+                    }
+
+                } else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
